cd7313c32693e61e152aa7e8ccee99afd8822f88
Hamza <yourgitemail@example.com>
2022-02-14 21:25:04 +0100

    Initial Commit

---
+++ FruitClassification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FruitClassification
+//
+//  Created by Hamza on 2/14/22.
+//  Copyright © 2022 Hamza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
